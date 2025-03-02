5464ea65b5e4a1f0b303a79bc385d40f3bea5234
Zachary Corvidae <zach@sneakycrow.dev>
2025-03-01 20:53:59 -0800

    Add Parsing-related errors to StreamError

--- src/error/queue.rs
+++ src/error/queue.rs
@@ -1,3 +1,6 @@
+use async_nats::jetstream::consumer::pull::BatchErrorKind;
+use axum::{http, response::IntoResponse};
+use http::StatusCode;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -8,6 +11,18 @@ pub enum QueueError {
 
 #[derive(Error, Debug)]
 pub enum StreamError {
+    #[error("Error parsing event time offset")]
+    ParseTimeOffset(#[from] time::error::ComponentRange),
+    #[error("Error parsing event")]
+    ParseEvent(#[from] serde_json::Error),
+    #[error("NATS stream batch error")]
+    Batch(#[from] async_nats::error::Error<BatchErrorKind>),
     #[error("Invalid Connection: {0}")]
     InvalidConnection(String),
+}
+
+impl IntoResponse for StreamError {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
+    }
 }
